23a5636b0b7b94cbd9ff9db8ec95b870fbe96954
Samad <Samad.o@outlook.com>
2026-04-26 14:48:13 -0400

    interval id and interval clear placeholders to ensure proper time keeping

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ const play = document.getElementById("play");
 const mySong = document.getElementById("mySong");
 const fullTime = document.getElementById("fullTime");
 const intervalTime = document.getElementById("intervalTime");
-const parseInterval = 0;
+let parseInterval = 0;
+let interval;
 
 play.addEventListener("click", () => {
   if (!mySong.paused) {
+    clearInterval(interval);
     mySong.pause();
   } else {
     mySong.play();
-    setInterval(() => {
+    interval = setInterval(() => {
       parseInterval++;
       intervalTime.innerText = parseInterval;
     }, 1000);
