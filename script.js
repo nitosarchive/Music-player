17ee675a3f8d745a6bd23339014db90118b83779
Samad <Samad.o@outlook.com>
2026-04-26 14:43:57 -0400

    display song length and incrementing time

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const play = document.getElementById("play");
 const mySong = document.getElementById("mySong");
+const fullTime = document.getElementById("fullTime");
+const intervalTime = document.getElementById("intervalTime");
+const parseInterval = 0;
 
 play.addEventListener("click", () => {
   if (!mySong.paused) {
     mySong.pause();
   } else {
     mySong.play();
+    setInterval(() => {
+      parseInterval++;
+      intervalTime.innerText = parseInterval;
+    }, 1000);
   }
 });
+
+window.onload = () => {
+  fullTime.innerText = Math.floor(mySong.duration / 60);
+};
