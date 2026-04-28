ecb0e09215f8e61d0144b1c0481a1e9535ea3feb
Samad <Samad.o@outlook.com>
2026-04-27 21:41:14 -0400

    added song list and skip button now plays next song dynamically.

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let time = 0;
 let interval;
 let minute;
 let seconds;
+let songIndex = 0;
 
 play.addEventListener("click", () => {
   if (!mySong.paused) {
@@ -32,34 +33,41 @@ window.onload = () => {
   fullTime.innerText = Math.floor(mySong.duration / 60);
 };
 
-mySongList = [];
-
 const mySongInventory = [
   {
-    Artist: "future",
-    Song: "low life",
+    artist: "Don Toliver",
+    song: "TMU",
+    src: "./audio/Don Toliver - TMU.mp3",
+  },
+  {
+    artist: "future ft. The Weeknd",
+    song: "low life",
     src: "./audio/Future - Lowlife ft The Weeknd.mp3",
+  },
+  {
+    artist: "goatye feat. kimbra",
+    song: "Somebody That I Used To Know",
+    src: "./audio/Gotye - Somebody That I Used To Know (feat. Kimbra) Official Music Video (1).mp3",
+  },
+  {
+    artist: "robert miles",
+    song: "children",
+    src: "./audio/Robert Miles - Children Dream Version.mp3",
   },
 ];
 
 document.getElementById("nextSong").addEventListener("click", () => {
-  mySong.src = mySongInventory[0].src;
+  if (songIndex === mySongInventory) {
+    songIndex = 0;
+  } else {
+    songIndex++;
+  }
 
+  console.log(songIndex);
+
+  mySong.src = mySongInventory[songIndex].src;
+  if (mySong.oncanplay) {
+    fullTime.innerText = Math.floor(mySong.duration / 60);
+  }
   mySong.play();
 });
-
-const inventory = [
-  { name: "asparagus", type: "vegetables", quantity: 9 },
-  { name: "bananas", type: "fruit", quantity: 5 },
-  { name: "goat", type: "meat", quantity: 23 },
-  { name: "cherries", type: "fruit", quantity: 12 },
-  { name: "fish", type: "meat", quantity: 22 },
-];
-
-const restock = { restock: true };
-const sufficient = { restock: false };
-const result = Map.groupBy(inventory, ({ quantity }) =>
-  quantity > 6 ? restock : sufficient,
-);
-console.log(result.get(restock));
-// [{ name: "bananas", type: "fruit", quantity: 5 }]
