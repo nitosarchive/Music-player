480ace6b1272f9cb5b75b2d017895f8f6433b5ef
Samad <Samad.o@outlook.com>
2026-05-26 19:45:25 -0400

    code refactor

--- script.js
+++ script.js
@@ -67,17 +67,11 @@ window.addEventListener("keyup", (e)=>{
 } )
 
 function totalTime() {
-  isTotalTime = false;
-  setInterval(() => {
-  if(isTotalTime === true ) return;
-  if(mySong.duration){
+  mySong.onload =
   totalMinutes = Math.floor(mySong.duration / 60);
   totalSeconds = Math.floor(mySong.duration - totalMinutes * 60);
-  
-  fullTime.innerText = `${totalMinutes.toString().padStart("1", "0")}:${totalSeconds.toString().padStart("2", "0")}`;}
-  if(mySong.duration) isTotalTime = true;
-  }, 200);
-  
+  fullTime.innerText = `${totalMinutes.toString().padStart("1", "0")}:${totalSeconds.toString().padStart("2", "0")}`;
+
 }
 
 
